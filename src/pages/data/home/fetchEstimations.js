87d79001efa6c6505c89040299e93f1f541a7036
camm852 <camm852@gmail.com>
2024-04-12 19:44:40 -0500

    Interceptor and page view

--- src/pages/data/home/fetchEstimations.js
+++ src/pages/data/home/fetchEstimations.js
@@ -1,43 +1,38 @@
-import axios from "axios";
+import { baseApi } from "../../config/axiosConfig";
 import { API_URL } from "../../constants/api";
 
 export const fetchEstimations = async (task) => {
-    try {
-      const response = await axios.get(`${API_URL}/API/chat?task=${encodeURIComponent(task)}`)
-      const data = await response.data;
-      console.log("data:", data);
-      return data;
-    } catch (error) {
-      console.error("Error fetching estimations:", error);
-      throw new Error("Error fetching estimations");
-    }
-  }
-
-
-export const setRating = async (id,newRating) => {
-
   try {
-      const response = await fetch(
-          `${API_URL}/API/estimations/${id}`,
-          {
-              method: 'POST',
-              body: JSON.stringify({
-                  'stars': newRating,
-              }),
-              headers: {
-                  "Content-Type": "application/json",
-              },
-          }
-      );
-      if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
+    const response = await baseApi.get(
+      `${API_URL}/API/chat?task=${encodeURIComponent(task)}`
+    );
+    const data = await response.data;
+    console.log("data:", data);
+    return data;
   } catch (error) {
-      console.error('Error fetching estimations:', error);
-      throw error;
+    console.error("Error fetching estimations:", error);
+    throw new Error("Error fetching estimations");
   }
+};
 
+export const setRating = async (id, newRating) => {
+  try {
+    const response = await fetch(`${API_URL}/API/estimations/${id}`, {
+      method: "POST",
+      body: JSON.stringify({
+        stars: newRating,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Error fetching estimations:", error);
+    throw error;
+  }
 
   // pendiente: llamar API para guardar el valor
-}
+};
--- src/router.jsx
+++ src/router.jsx
@@ -1,18 +1,23 @@
 import App from "./App";
-import {createBrowserRouter} from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom";
 import HistoryPage from "./pages/view/history";
 import { About } from "./pages/view/About";
+import Profile from "./pages/profile";
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-    },
-    {
-      path: "history/:idUser",
-      element: <HistoryPage />,
-    },
-    {
-      path: "credits",
-      element: <About />
-    },
-  ]);
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "history/:idUser",
+    element: <HistoryPage />,
+  },
+  {
+    path: "credits",
+    element: <About />,
+  },
+  {
+    path: "profile",
+    element: <Profile />,
+  },
+]);
